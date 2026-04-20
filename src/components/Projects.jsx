89c9ebef199533b5a75e1d06d169aa8b9dc2dbc2
theAleXX <th3alexx@gmail.com>
2026-04-20 18:33:58 +0200

    correct Skills section

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,16 +1,12 @@
 import { motion } from 'framer-motion';
 import analogClock from '../assets/analog-clock-proj.png';
 import converter from '../assets/converter-proj.png';
+import generator from '../assets/generator-proj.png';
+import countdown from '../assets/countdown-proj.png';
+import reactImg from '../assets/react.png';
 
 const Projects = () => {
   const works = [
-    {
-      title: 'Analog Clock JS',
-      img: analogClock,
-      url: 'https://codepen.io/editor/FideltheAleXX/pen/019da4a0-1f8f-7f6c-9db3-f6d66f0190bd',
-      description:
-        'A JavaScript-based analog clock demonstrating real-time rendering, DOM updates, and CSS transform usage.',
-    },
     {
       title: 'Currency Converter',
       img: converter,
@@ -24,6 +20,32 @@ const Projects = () => {
       url: 'https://codepen.io/editor/FideltheAleXX/pen/019da4a0-1f8f-7f6c-9db3-f6d66f0190bd',
       description:
         'A JavaScript-based analog clock demonstrating real-time rendering, DOM updates, and CSS transform usage.',
+    },
+    {
+      title: 'Generator the Passwords',
+      img: generator,
+      url: 'https://fidelthealexx.github.io/generatoe-the-passwords/',
+      description:
+        'A lightweight JavaScript web application for generating strong, randomized passwords with customizable length and character types.',
+    },
+    {
+      title: 'Countdown',
+      img: countdown,
+      url: 'https://fidelthealexx.github.io/new-year-countdown/',
+      description:
+        'A real-time JavaScript countdown clock designed to holiday events with automatic interval management.',
+    },
+    {
+      title: 'Landing-page for cosmetologist',
+      img: reactImg,
+      url: 'https://github.com/FideltheAleXX/landing-cosmetolog',
+      description: 'Landing page for cosmetologist.',
+    },
+    {
+      title: 'React mp-3 Player',
+      img: reactImg,
+      url: 'https://github.com/FideltheAleXX/mp3-player-react',
+      description: 'MP3 player use React JS.',
     },
   ];
   return (
