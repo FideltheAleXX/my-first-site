120756c3f851b56eabaeacc2ca44e1196fd7cad0
theAleXX <th3alexx@gmail.com>
2026-04-27 19:35:14 +0200

    correct homepage

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'https://FideltheAleXX.github.io/my-first-site/',
+  base: '/',
   plugins: [react()],
 });
